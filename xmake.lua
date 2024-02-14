f85f7ed64c8a3fcaf6198020c9fdbc34d45d9809
daoge_cmd <3523206925@qq.com>
2024-02-15 04:14:29 +0800

    feat: use ll 0.8.1

--- xmake.lua
+++ xmake.lua
@@ -8,39 +8,33 @@ end
 
 add_requires("zlib-ng 2.1.3")
 
--- Option 1: Use the latest version of LeviLamina released on GitHub.
--- add_requires("levilamina")
+-- Latest release
+add_requires("levilamina")
 
--- Option 2: Use a specific version of LeviLamina released on GitHub.
--- add_requires("levilamina x.x.x")
-
--- Option 3: Use the latest commit of LeviLamina on GitHub.
--- Here, "develop" is the branch name. You can change it to any branch name you want.
-add_requires("levilamina develop")
--- You can also use debug build of LeviLamina.
--- add_requires("levilamina develop", {debug = true})
-package("levilamina")
-    add_urls("https://github.com/LiteLDev/LeviLamina.git")
-
-    add_deps("ctre 3.8.1")
-    add_deps("entt 3.12.2")
-    add_deps("fmt 10.1.1")
-    add_deps("gsl 4.0.0")
-    add_deps("leveldb 1.23")
-    add_deps("magic_enum 0.9.0")
-    add_deps("nlohmann_json 3.11.2")
-    add_deps("rapidjson 1.1.0")
-    add_deps("pcg_cpp 1.0.0")
-    add_deps("pfr 2.1.1")
-    add_deps("preloader 1.4.0")
-    add_deps("symbolprovider 1.1.0")
-
-    -- You may need to change this to the target BDS version of your choice.
-    add_deps("bdslibrary 1.20.61.01")
-
-    on_install(function (package)
-        import("package.tools.xmake").install(package)
-    end)
+-- Latest commit on develop
+-- add_requires("levilamina develop")
+-- package("levilamina")
+--     add_urls("https://github.com/LiteLDev/LeviLamina.git")
+--
+--     add_deps("ctre 3.8.1")
+--     add_deps("entt 3.12.2")
+--     add_deps("fmt 10.1.1")
+--     add_deps("gsl 4.0.0")
+--     add_deps("leveldb 1.23")
+--     add_deps("magic_enum 0.9.0")
+--     add_deps("nlohmann_json 3.11.2")
+--     add_deps("rapidjson 1.1.0")
+--     add_deps("pcg_cpp 1.0.0")
+--     add_deps("pfr 2.1.1")
+--     add_deps("preloader 1.4.0")
+--     add_deps("symbolprovider 1.1.0")
+--
+--     -- You may need to change this to the target BDS version of your choice.
+--     add_deps("bdslibrary 1.20.61.01")
+--
+--     on_install(function (package)
+--         import("package.tools.xmake").install(package)
+--     end)
 
 target("DataExtractor") -- Change this to your plugin name.
     add_cxflags(
